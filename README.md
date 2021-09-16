ae9afdf7d717bd10eb2c76d53d90e576191b7cdf
Joseph McDade <JMcDade42@gmail.com>
2021-09-16 12:54:35 -0500

    Templates and Bootstrap

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Project for learning and playing with a Flask webserver
+
+Following series ["Python Flask Tutorial: Full-Featured Web App" by Corey Schafer](https://www.youtube.com/playlist?list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH)
--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+# Test posts
+posts = [
+  {
+    'author': 'Jebediah Kerman',
+    'title': 'Snacks',
+    'content': "We didn't bring enough snacks.",
+    'date_posted': 'Aug 10, 2021'
+  },
+  {
+    'author': 'Valentina Kerman',
+    'title': 'Piloting',
+    'content': "Pro tip: Keep the pointy end up.",
+    'date_posted': 'Aug 15, 2021'
+  }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello World!</h1>"
+  return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About</h1>"
+  return render_template('about.html', title="About")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
