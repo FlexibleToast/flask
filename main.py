bd465e7d11b345a030da4097699185b3d8a8dbdc
Joseph McDade <JMcDade42@gmail.com>
2021-09-16 15:13:49 -0500

    Forms

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+# Should be an environment variable
+app.config['SECRET_KEY'] = '6bbec13239799c10b4e62df86679bb7e0fc65bc1'
 
 # Test posts
 posts = [
@@ -26,6 +30,25 @@ def home():
 @app.route("/about")
 def about():
   return render_template('about.html', title="About")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    flash(f'Account created for {form.username.data}!', 'success')
+    return redirect(url_for('home'))
+  return render_template('register.html', title="Register", form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+    if form.email.data == 'JMcDade42@gmail.com' and form.password.data =='itsasecret':
+      flash(f'{form.email.data} successfully logged in!', 'success')
+      return redirect(url_for('home'))
+    else:
+      flash('Login Failed', 'danger')
+  return render_template('login.html', title="Login", form=form)
 
 if __name__ == '__main__':
   app.run(debug=True)
